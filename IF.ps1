923eb8efec24e003d24f4362b6c8b071eb3364c6
L00179097 <l00179097@atu.ie>
2024-12-23 02:48:00 +0000

    IF ELSE

---
+++ IF.ps1
@@ -0,0 +1,8 @@
+$Variable1=31
+$Variable2=32
+
+if ($Variable1 -eq $Variable2) {
+    Write-Output "The condition was false: Both variables are equal"
+} else {
+    Write-Output "The condition was true: Variables are not equal"
+}
