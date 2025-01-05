e2bb57282ef13bdd396ddcb6f9e3992bcea6a102
L00179097 <l00179097@atu.ie>
2025-01-05 00:05:35 +0000

    Install AD-Domain

---
+++ Remote Control DC1 & DC2/Build_AD.ps1
@@ -0,0 +1,17 @@
+Install-WindowsFeature -name AD-Domain-Services –IncludeManagementTools
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "l00179097.ie" `
+-DomainNetbiosName "l00179097" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
+Shutdown /r /t 0
